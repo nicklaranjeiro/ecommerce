a4445e3766521efa901c3fc634beb4e4ba4314cc
nicklaranjeiro <nicholaslaranjeiro@hotmail.com>
2021-01-19 16:44:23 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [ products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+        setProducts(data);
+    }
+
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+        fetchCart();
+    }, []);
+
+    console.log(products);
+
+    return (
+        <Router>
+            <div>
+                <Navbar totalItems = {cart.total_items} />
+                <Switch>
+                    <Route exact path="/">
+                        <Products products={products} onAddToCart={handleAddToCart} />
+                    </Route>
+
+                    <Route exact path="/cart">
+                        <Cart cart={cart} /> 
+                    </Route>               
+                </Switch>
+                
+            </div>    
+        </Router>        
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuUtem, Menu, Typogrpahy, Typography} from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+
+import logo from '../../assets/store.png';
+import useStyles from './styles';
+
+const Navbar = ( {totalItems}) => {
+    const classes = useStyles();
+    return (
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src={logo} alt="logo" height="25px" className={classes.image}/>
+                        XStock
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton aria-label="Show cart items" color="inherit">
+                            <Badge badgeContent={totalItems} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar
